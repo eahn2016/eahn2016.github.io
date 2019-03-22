1cf2c167955f1d7c2d4c24a259232a1806ad6f07
Eugene Ahn <eahn2016@gmail.com>
2019-03-21 23:55:01 -0400

    Finished gameboy page

--- js/creative.js
+++ js/creative.js
@@ -40,7 +40,7 @@
   $(window).scroll(navbarCollapse);
 
   // Magnific popup calls
-  // $('#portfolio').magnificPopup({
+  // $('#popfolio').magnificPopup({
   //   delegate: 'a',
   //   type: 'image',
   //   tLoading: 'Loading image #%curr%...',
